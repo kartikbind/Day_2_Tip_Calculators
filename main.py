af3f05fccb1dd6e335616ce978a2ec31e6b75372
kartikbind <kartikbind@gmail.com>
2022-03-04 18:47:41 +0530

    Day 2 Day 2 of 100 Days of code Python code Course end project: Tip Calculator

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ no_of_people = float(input("How many people to split the bill? "))
 percent_tip = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
 amount_with_tip = total_bill_amount + (total_bill_amount * percent_tip / 100)
 amount_per_person = amount_with_tip / no_of_people
-print("Each person should pay: $", amount_per_person)
+print(f"Each person should pay: ${amount_per_person}")
